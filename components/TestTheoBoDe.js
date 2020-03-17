43677f240165172fa140c8a0b7f96c4e9c15b622
hoang minh <hoangminh12297@gmail.com>
2020-03-17 23:55:58 +0700

    minh.hoang/fix-error-A1/17/3/2020

--- components/TestTheoBoDe.js
+++ components/TestTheoBoDe.js
@@ -8,9 +8,10 @@ class HorizontalFlatListItem extends Component {
     constructor(props){
         super(props);
         this.state={
-            check1:true,
+            check1:false,
             check2:false,
-            check3:false
+            check3:false,
+            keyChange:0
         }
     }
     //kiem tra check1,check2,... ddungs khong
@@ -31,40 +32,47 @@ class HorizontalFlatListItem extends Component {
        }
          }
     }
-    checkOnChange =(index) =>{
+    checkOnChange =(key,index) =>{
         switch(index){
             case 0:{
                 this.setState({
-                     check1 : !this.state.check1
+                     check1 : !this.state.check1,
+                     keyChange:key
                  });
                  console.log(this.state.check1)
                  break;
             }
             case 1:{
                 this.setState({
-                    check2 : !this.state.check2
+                    check2 : !this.state.check2,
+                    keyChange:key
                 });
                break;
           }
           case 2:{
                 this.setState({
-                    check3 : !this.state.check3
+                    check3 : !this.state.check3,
+                    keyChange:key
                 });
                 break;
           }
         }
-        console.log("ok1")
+        // console.log("key:" +key)
+        //save answer change
+      
     }
     render(){
-        console.log("inde"+this.props.index)
+        this.props.parentFlatList.getAnswerUser(this.state.keyChange,this.state.check1,this.state.check2,this.state.check3)
+        console.log("check con:" +this.state.check1,this.state.check2,this.state.check3);
+        // console.log("inde"+this.props.index)
 
         // console.log(this.checkValueIndex(1))
        if(this.props.item.image ==""){
             return (
-                <View key={this.props.index} style={styles.horizontalParent}>
+                <View  style={styles.horizontalParent}>
                 <Text style={styles.itemTitle}>{this.props.item.title}</Text>
-                    <View style={{margin:5}}>
-                    {this.props.item.result.map((val,index) => <View style={{flexDirection:'row'}}><CheckBox value={this.checkValueIndex(index)}  onChange = {() => this.checkOnChange(index) } ></CheckBox><Text style={{marginTop:5}}>{val}</Text></View>)} 
+                    <View key={this.props.index} style={{margin:5}}>
+                    {this.props.item.result.map((val,index) => <View style={{flexDirection:'row'}}><CheckBox value={this.checkValueIndex(index)}  onChange = {() => this.checkOnChange(this.props.index,index) } ></CheckBox><Text style={{marginTop:5}}>{val}</Text></View>)} 
                     </View>
                 </View>
             )
@@ -91,11 +99,38 @@ class HorizontalFlatListItem extends Component {
 }
 
 export default class componentName extends Component {
+     answerUser = new Map();
     constructor(props){
         super(props)
-        this.state={
-            answerUser:[]
-        }
+        // this.state={
+        //     answerUser:[]
+        // }
+        
+
+    }
+    changeBoolenToInt = (val) =>{
+         if(val ==true) return 1;
+         else 
+            return 0;
+         return 0;
+    }
+    getAnswerUser =(key,check1,check2,check3) =>{
+            let arr=[];
+            arr.push(this.changeBoolenToInt(check1));
+            arr.push(this.changeBoolenToInt(check2));
+            arr.push(this.changeBoolenToInt(check3));
+            // this.answerUser.push({key:})
+            this.answerUser.set(key,arr);
+            //tim vi tri 
+            // this.answerUser.map((value,index) =>{
+            //     if(key == index) 
+            // });
+            // console.log("m:"+this.answerUser.size)
+            console.log("check cha:"+check1+check2+check3)
+            this.answerUser.forEach(function(value, key, map) {
+                console.log(`${key} has ${value}`)
+            });
+          
     }
    
   render() {
@@ -107,7 +142,7 @@ export default class componentName extends Component {
                 horizontal={true}
                 renderItem = {({item,index}) =>{
                         return (
-                        <HorizontalFlatListItem image={item.image} index={index} item={item} parentFlatList={this}>
+                        <HorizontalFlatListItem  image={item.image} index={index} item={item} parentFlatList={this}>
                         </HorizontalFlatListItem>
                         );
                 }}
